535328386ddfb7085e73580980a129e9f49805b9
Jorge Rodriguez <jorg1981@gmail.com>
2022-07-03 11:38:32 -0300

    modificamos la tura del sw

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+const url = window.location.href;
+const seLocation = "/twittor/sw.js";
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js");
+  if (url.includes("localhost")) {
+    swLocation = "/sw.js";
+  }
+  navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
